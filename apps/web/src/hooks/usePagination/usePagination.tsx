d3afbc4c3eea0f2fb21afa93a56db8ef1c8d9da7
Nikola Krezeski <nkrezeski@banzae.dev>
2023-12-03 01:23:50 +0100

    ref: change initial categories count

--- apps/web/src/hooks/usePagination/usePagination.tsx
+++ apps/web/src/hooks/usePagination/usePagination.tsx
@@ -2,7 +2,7 @@ import { useCallback, useState } from 'react'
 
 export const usePagination = () => {
   const [pageParam, setPageParam] = useState(1)
-  const [pageSize, setPageSize] = useState(10)
+  const [pageSize, setPageSize] = useState(5)
 
   const incrementPage = useCallback(() => setPageParam((prev) => prev + 1), [])
 
